02b1fb1c15c42b94bf67e35a6ed4d5af5a59aaa7
Oscar Carballal Prego <info@oscarcp.com>
2014-09-19 18:46:47 +0100

    Minor changes

--- assets/js/directives/addobject.js
+++ assets/js/directives/addobject.js
@@ -4,19 +4,41 @@ app.directive("modalDialog", function(){
     return {
         restrict: 'A',
         templateUrl: 'includes/add-item.html',
-        controllerAs: 'addiotem',
+        controllerAs: 'additem',
         controller: function($http, $scope) {
             $http.get('http://localhost:8080/v1/add')
             .success(function(data, status, headers, config) {
                 //$.notify("Data saved successfully", "success");
             })
             .error(function(data, status, headers, config) {
-                $.notify("Can't access the API. We will disable the add functionality", "error");
+                $.notify("Can't access the API. Adding items is disabled", "error");
                 // TODO: Decide if disabling the button or remove it completely
-                //$(".additem").addClass("disabled");
-                //$(".additem").prop("disabled", true);
+                $(".additem").addClass("disabled");
+                $(".additem").prop("disabled", true);
                 // This removes the button
                 //$(".additem").remove();
+            });
+        }
+    };
+});
+
+app.directive("addService", function(){
+    return {
+        restrict: 'A',
+        templateUrl: 'includes/add-service.html',
+        controllerAs: 'addobject',
+        controller: function($http, $scope) {
+            $http.get('http://localhost:8080/v1/add')
+            .success(function(data, status, headers, config) {
+                //$.notify("Data saved successfully", "success");
+            })
+            .error(function(data, status, headers, config) {
+                $.notify("Can't access the API. Adding services is disabled", "error");
+                // TODO: Decide if disabling the button or remove it completely
+                $(".addobject").addClass("disabled");
+                $(".addobject").prop("disabled", true);
+                // This removes the button
+                $(".addobject").remove();
             });
         }
     };
