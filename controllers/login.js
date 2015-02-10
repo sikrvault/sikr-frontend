21ee9276f38f779ed57df2dab142791b7752c504
Oscar Carballal Prego <info@oscarcp.com>
2015-02-10 19:03:55 +0000

    Removed old code

--- controllers/login.js
+++
@@ -1,9 +0,0 @@
-// Login
-app.controller('LoginCtrl', function($scope, $auth) {
-    $scope.login = function() {
-      $auth.login({ email: $scope.email, password: $scope.password });
-    };
-    $scope.authenticate = function(provider) {
-      $auth.authenticate(provider);
-    };
-});
