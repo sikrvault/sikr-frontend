30c06516bd3897f8f76a4f0254eea920e705a6ea
Oscar Carballal Prego <info@oscarcp.com>
2015-02-13 11:31:22 +0000

    More tests..

--- assets/js/app.js
+++ assets/js/app.js
@@ -85,7 +85,7 @@ app.config(function($authProvider) {
         clientId: '2d769d69d5106d8838ee',
         //name: 'Sikre',
         //url: '/v1/auth/github/login',
-        //redirectUri: 'https://api.sikr.io/v1/auth/github/login',
+        redirectUri: 'https://api.sikr.io/v1/auth/github/login',
         //redirectUri: window.location.origin || window.location.protocol + '//' + window.location.host,
         //authorizationEndpoint: 'https://github.com/login/oauth/authorize',
         //scope: [],
