25ba7a370de7da5d510e4a4a798e261f5e31bb9c
Oscar Carballal Prego <info@oscarcp.com>
2015-02-23 18:09:46 +0000

    More tests for the logout

--- assets/js/controllers.js
+++ assets/js/controllers.js
@@ -19,12 +19,9 @@ angular.module('sikre.controllers', [])
     };
   })
 
-  .controller('LogoutCtrl', function ($auth) {
-    if (!$auth.isAuthenticated()) {
-      return;
-    }
-    $auth.logout()
-      .then(function () {
+  .controller('LogoutCtrl', function ($scope, $auth) {
+    $scope.logout = function () {
+        $auth.logout();
         $.notify("You have been logged out", "error");
       });
   });
