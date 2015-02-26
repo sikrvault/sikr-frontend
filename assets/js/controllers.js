7577ff06ccaf8ae542c2c34e3c576a09a7a27532
Oscar Carballal Prego <info@oscarcp.com>
2015-02-26 18:25:17 +0000

    Multiple changes to the items and groups

--- assets/js/controllers.js
+++ assets/js/controllers.js
@@ -14,13 +14,15 @@ angular.module('sikre.controllers', [])
 
   .controller('GroupsCtrl', function ($scope, sikreAPIservice) {
 
-    sikreAPIservice.getGroups()
-      .success(function (response) {
-        $scope.groupList = response;
-      })
-      .error(function () {
-        $.notify("Can't access the API to get the groups.", "error");
-      });
+    $scope.getgroups = function () {
+      sikreAPIservice.getGroups()
+        .success(function (response) {
+          $scope.groupList = response;
+        })
+        .error(function () {
+          $.notify("Can't access the API to get the groups.", "error");
+        });
+    }
 
     $scope.update = function () {
       sikreAPIservice.getGroups()
@@ -47,6 +49,7 @@ angular.module('sikre.controllers', [])
         .success(function () {
           $.notify("Group saved", "success");
           $scope.group = null;
+          $('#addItem').foundation('reveal', 'close');
         })
         .error(function () {
           $.notify("Can't save the group", "error");
@@ -76,14 +79,6 @@ angular.module('sikre.controllers', [])
 
   .controller('ItemsCtrl', function ($scope, sikreAPIservice) {
 
-    // sikreAPIservice.getItems()
-    //   .success(function (response) {
-    //     $scope.itemList = response;
-    //   })
-    //   .error(function () {
-    //     $.notify("Can't access the API to get the items.", "error");
-    //   });
-
     $scope.getitemgroups = function () {
       sikreAPIservice.getGroups()
         .success(function (response) {
@@ -109,6 +104,7 @@ angular.module('sikre.controllers', [])
         .success(function () {
           $.notify("Item saved", "success");
           $scope.item = null;
+          $('#addItem').foundation('reveal', 'close');
         })
         .error(function () {
           $.notify("Can't save the item", "error");
@@ -133,54 +129,5 @@ angular.module('sikre.controllers', [])
         .error(function () {
           $.notify("Can't delete item", "error");
         });
-    };
-  })
-
-  .controller('groupsController', function ($scope, sikreAPIservice) {
-
-    sikreAPIservice.getGroups()
-      .success(function (response) {
-        $scope.groupList = response;
-      })
-      .error(function () {
-        $.notify("Can't access the API to get the groups.", "error");
-      });
-  })
-
-  .controller('addItem', function ($scope, sikreAPIservice) {
-
-    /*
-      Get the groups that belong to the user and populate the groupList
-      variable so we can populate the dropdown.
-    */
-    sikreAPIservice.getGroups()
-      .success(function (response) {
-        $scope.groupList = response;
-      })
-      .error(function () {
-        $.notify("Can't access the API to get the groups.", "error");
-      });
-
-    /*
-      Create an update function that will post the form data back to the API.
-      If successful notify the user and close the modal dialog, otherwise
-      show an error message
-    */
-    $scope.update = function (item) {
-      sikreAPIservice.saveItem(item)
-        .success(function () {
-          $.notify("Item saved successfully", "success");
-          $('#addItem').foundation('reveal', 'close');
-        })
-        .error(function () {
-          $.notify("Can't save the item", "error");
-        });
-    };
-
-    /*
-      Reset the contents of the form
-    */
-    $scope.reset = function () {
-      $scope.item = {};
     };
   });
