aa9645ea20e4e9bde5b97eb8756dfe10414ed878
Oscar Carballal Prego <info@oscarcp.com>
2015-04-15 15:51:06 +0100

    Change open method for the offcanvas

--- assets/js/app.js
+++ assets/js/app.js
@@ -118,7 +118,7 @@ app.run(function ($timeout) {
       offcanvas : {
         // Sets method in which offcanvas opens.
         // [ move | overlap_single | overlap ]
-        open_method: 'move',
+        open_method: 'overlap',
         // Should the menu close when a menu link is clicked?
         // [ true | false ]
         close_on_click : true
