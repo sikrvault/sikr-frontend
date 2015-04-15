561c261f92b96dc41ed2777508a30bcead11a65d
Oscar Carballal Prego <info@oscarcp.com>
2015-04-15 15:55:38 +0100

    Overlap method

--- assets/js/app.js
+++ assets/js/app.js
@@ -124,7 +124,7 @@ app.run(function ($timeout) {
         close_on_click : true
       }
     });
-    $('.off-canvas-wrap').foundation('offcanvas', 'toggle', 'move-right');
+    $('.off-canvas-wrap').foundation('offcanvas', 'toggle', 'overlap');
 
   }, 500);
 });
